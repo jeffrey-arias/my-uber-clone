e49dfe53f512e478fee0ac8de1862a536669551d
jeffrey-arias <jeffrey.e.arias@gmail.com>
2023-01-15 01:36:13 +0800

    Renames

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { View, Text, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
 
-export default function Home() {
+export default function HomeScreen() {
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 
-export default function Map() {
+export default function MapScreen() {
     return (
         <SafeAreaView>
             <View>
