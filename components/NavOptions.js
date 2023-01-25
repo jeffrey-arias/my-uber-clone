32c46a176c97d331def555e54ece5645c42761ac
jeffrey-arias <jeffrey.e.arias@gmail.com>
2023-01-26 00:49:16 +0800

    Add disabled buttons

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,7 +2,9 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { useSelector } from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
+import { selectOrigin } from '../slices/navSlice';
 
 const data = [
     {
@@ -21,6 +23,7 @@ const data = [
 
 export default function NavOptions() {
     const navigation = useNavigation();
+    const origin = useSelector(selectOrigin);
     return (
         <FlatList
             data={data}
@@ -32,8 +35,9 @@ export default function NavOptions() {
                         navigation.navigate(item.screen);
                     }}
                     style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                    disabled={!origin}
                 >
-                    <View>
+                    <View style={tw`${!origin && 'opacity-20'}`}>
                         <Image
                             style={{
                                 height: 120,
@@ -42,16 +46,16 @@ export default function NavOptions() {
                             }}
                             source={{ uri: item.image }}
                         />
+                        <Text style={tw`mt-2 text-lg font-semibold`}>
+                            {item.title}
+                        </Text>
+                        <Icon
+                            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                            name="arrowright"
+                            color="white"
+                            type="antdesign"
+                        />
                     </View>
-                    <Text style={tw`mt-2 text-lg font-semibold`}>
-                        {item.title}
-                    </Text>
-                    <Icon
-                        style={tw`p-2 bg-black rounded-full w-10 mt-4`}
-                        name="arrowright"
-                        color="white"
-                        type="antdesign"
-                    />
                 </TouchableOpacity>
             )}
         />
