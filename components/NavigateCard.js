f224b801d7f20cf716eb2ae011f5c7223c92d4b5
jeffrey-arias <jeffrey.e.arias@gmail.com>
2023-01-26 01:18:36 +0800

    Added destination screen

---
+++ components/NavigateCard.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { View } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import tw from 'tailwind-react-native-classnames';
+import { GOOGLE_MAPS_API_KEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination } from '../slices/navSlice';
+import { useNavigation } from '@react-navigation/native';
+
+export default function NavigateCard() {
+    const dispatch = useDispatch();
+    const navigation = useNavigation();
+    return (
+        <SafeAreaView style={tw`bg-white flex-1`}>
+            <Text style={tw`text-center py-5 text-xl`}>Hello, Iza</Text>
+            <View style={tw`border-t border-gray-200 flex-shrink`}>
+                <GooglePlacesAutocomplete
+                    placeholder="Where to?"
+                    debounce={400}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    returnKeyType={'search'}
+                    fetchDetails={true}
+                    enablePoweredByContainer={false}
+                    styles={toInputBoxStyles}
+                    minLength={2}
+                    query={{
+                        key: GOOGLE_MAPS_API_KEY,
+                        language: 'en',
+                    }}
+                    onPress={(data, details = null) => {
+                        dispatch(
+                            setDestination({
+                                location: details.geometry.location,
+                                description: data.description,
+                            })
+                        );
+                        navigation.navigate('RideOptionsCard');
+                    }}
+                />
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        paddingTop: 20,
+        flex: 0,
+    },
+    textInput: {
+        backgroundColor: '#DDDDDF',
+        borderRadius: 0,
+        fontSize: 18,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0,
+    },
+});
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,37 @@
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
+import NavigateCard from '../components/NavigateCard';
+import RideOptionsCard from '../components/RideOptionsCard';
 
 export default function MapScreen() {
+    const Stack = createStackNavigator();
     return (
         <SafeAreaView>
             <View>
                 <View style={tw`h-1/2`}>
                     <Map />
                 </View>
-                <View style={tw`h-1/2`}></View>
+                <View style={tw`h-1/2`}>
+                    <Stack.Navigator>
+                        <Stack.Screen
+                            name="NavigateCard"
+                            component={NavigateCard}
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                        <Stack.Screen
+                            name="RideOptionsCard"
+                            component={RideOptionsCard}
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                    </Stack.Navigator>
+                </View>
             </View>
         </SafeAreaView>
     );
